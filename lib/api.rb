1224335aa639e37bbc8abcef520c31d20d7c8681
gilr00y <jas.gilbertson@gmail.com>
2014-07-05 21:45:18 -0400

    Build happy url

--- lib/api.rb
+++ lib/api.rb
@@ -1,9 +1,18 @@
+require 'patron'
+
 module Google
   module Elevation
     class Api
 
-      def self.blah
-        puts "BLAHAAHAHAHAH!!!!"
+      def api_key
+        @api_key ||= ENV['ELEVATION_KEY']
+      end
+
+      def get_elevation(latitude, longitude, format='json')
+        # location format: {lat,lng}
+        url = "https://maps.googleapis.com/maps/api/elevation/#{format}?key=#{api_key}&locations=#{latitude},#{longitude}"
+        session = Patron::Session.new
+        session.get(url).body
       end
 
     end
